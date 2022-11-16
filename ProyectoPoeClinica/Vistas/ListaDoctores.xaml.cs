952925bd1a8e27832c313f1243223dd8defdd248
Erick Cubias <3rickcubias@gmail.com>
2022-11-15 22:04:50 -0600

    Agregar Especialidades

--- ProyectoPoeClinica/Vistas/ListaDoctores.xaml.cs
+++ ProyectoPoeClinica/Vistas/ListaDoctores.xaml.cs
@@ -27,55 +27,28 @@ namespace ProyectoPoeClinica.Vistas
       
         public ListaDoctores()
         {
-            InitializeComponent();
-            
-
-            string miConexion = ConfigurationManager.ConnectionStrings["ProyectoPoeClinica.Properties.Settings.ClinicaConnectionString"].ConnectionString;
-            miConexionSql = new SqlConnection(miConexion);
-           // mostrarusuarios();
+            InitializeComponent();            
+           
           mostrarDoctores();
         }
-
-
-        /* private void mostrarusuarios()
-           {
-               string consulta = "select concat(Nombres, ' ', Apellidos,'', Nombre_Especialidad) as Info from Usuarios where Puesto like '%Medico%' ";
-               SqlDataAdapter adaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
-               using(miConexionSql)
-               {
-                   DataTable usuariosTabla = new DataTable();
-                   adaptadorSql.Fill(usuariosTabla);
-                   listaDoctores.DisplayMemberPath = "Info";
-                   listaDoctores.SelectedValuePath = "ID";
-                   listaDoctores.ItemsSource = usuariosTabla.DefaultView;
-               }
-           }*/
-
         private void mostrarDoctores()
         {
             
             using (Model.ClinicaEntities db = new Model.ClinicaEntities())
             {
-                 var lst = (from p in db.Usuarios
+                List<Doctoresview> list = new List<Doctoresview>();
+                
+                list = (from c in db.Usuarios
+                       where c.ID_Especialidad!=null
+                       
                        select new Doctoresview
                        {
-                           nombres=p.Nombres,
-                           apellidos=p.Apellidos
-
-                       })
-                       .Join((from b in db.Especialidad select new Especialidadview
-                       {
-                           especialiadad=b.Nombre_Especialidad
-                       }),
-                       ub=>ub.ID,
-                       eb=>eb.ID_Especialidad,
-                       (ub, eb) => new
-                       {
-                           ub.nombres,
-                           ub.apellidos,
-                           eb.especialiadad
+                           nombres=c.Nombres,
+                           apellidos=c.Apellidos,
+                           especialidad=c.Especialidad.Nombre_Especialidad
                        }).ToList();
-                Dg.ItemsSource = lst;
+                Dg.ItemsSource = list;
+
             }
             
         }
@@ -86,20 +59,16 @@ namespace ProyectoPoeClinica.Vistas
             public string alias { get; set; }
             public string contra { get; set; }
             public string puesto { get; set; }
-            public int id_rold { get; set; }
+            public int id_rol { get; set; }
             public int id_Especialidad { get; set; }
             public int ID { get; set; }
+            public string especialidad { get; set; }
 
 
         }
 
-        public class Especialidadview
-        {
-            public int ID { get; set; }
-            public string especialiadad { get; set; }
-            public int ID_Especialidad { get; set; }
-        }
+        
 
-        SqlConnection miConexionSql;
+        
     }
 }
